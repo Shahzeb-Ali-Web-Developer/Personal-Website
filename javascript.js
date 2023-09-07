0a486916ba6942466898a01a8aae20edb418f5c1
Shahzeb Ali <126076773+Shahzeb-Ali-Web-Developer@users.noreply.github.com>
2023-09-07 23:26:54 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+ let SubmitPopup = document.querySelector('.submit');
+ SubmitPopup.addEventListener('click', popup);
+
+ function popup(){
+    alert('Thank You, We will reach you within 24 Huors 🙂');
+ }
